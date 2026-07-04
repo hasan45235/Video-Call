a803fdf81fd06a8eece5b99fc47cf718a2d2eb50
Muhammad Hasan <muhammadhsn72@gmail.com>
2026-07-04 17:39:12 +0500

    turn servers added

--- src/services/peerConnection.js
+++ src/services/peerConnection.js
@@ -18,14 +18,43 @@
 const RTC_CONFIG = {
   iceServers: [
     {
-      urls: [
-        "stun:stun.l.google.com:19302",
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun3.l.google.com:19302",
-        "stun:stun4.l.google.com:19302",
-      ],
+      urls: "stun:stun.relay.metered.ca:80"
     },
+    {
+      urls: "stun:stun.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun1.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun2.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun3.l.google.com:19302"
+    },
+    {
+      urls: "stun:stun4.l.google.com:19302"
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80",
+      username: "a3dd80826130bc902c2073d1",
+      credential: "MKCPKvyQfeLkRP6U",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "a3dd80826130bc902c2073d1",
+      credential: "MKCPKvyQfeLkRP6U",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:443",
+      username: "a3dd80826130bc902c2073d1",
+      credential: "MKCPKvyQfeLkRP6U",
+    },
+    {
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "a3dd80826130bc902c2073d1",
+      credential: "MKCPKvyQfeLkRP6U",
+    }
   ],
 };
 
